2bb0104ba3ad170da36ba3caae82975fdbc5859b
Henrique Centieiro <32439646+Bitatlas@users.noreply.github.com>
2021-01-31 21:25:57 +0800

    Update myethashtest.py
    added print to print mining output

--- myethashtest.py
+++ myethashtest.py
@@ -7,7 +7,8 @@ from rlp import encode
 from eth_utils import to_bytes, to hex
 from web3 import IPCProvider, Web3
 
-_BYTES = 4                    # bytes in word 
+
+_BYTES = 4                        # bytes in word 
 DATASET_BYTES_INIT = 2**30        # bytes in dataset at genesis 
 DATASET_BYTES_GROWTH = 2**23      # dataset growth per epoch 
 CACHE_BYTES_INIT = 2**24          # bytes in cache at genesis 
@@ -19,6 +20,7 @@ HASH_BYTES = 64                   # hash length in bytes
 DATASET_PARENTS = 256             # number of parents of each dataset element 
 CACHE_ROUNDS = 3                  # number of rounds in cache production 
 ACCESSES = 64                     # number of accesses in hashimoto loop
+
 
 address = Binary.fixed_length(20, allow_empty=True)
 hash32 = Binary.fixed_length(32)
@@ -32,6 +34,7 @@ class MiningBlockHeader(rlp.Serializable):
         ('coinbase', address),
         ('state_root', trie_root),
         ('transaction_root', trie_root),
+        ('receipt_root', trie_root),
         ('bloom', uint256),
         ('difficulty', big_endian_int),
         ('block_number', big_endian_int),
@@ -65,5 +68,49 @@ blockNumber = int(sys.argv[1], 10)
      extra_data = to_bytes(int(w3.eth.getBlock(blockNumber).extraData.hex(), 16)),
      #mix_hash = to_bytes(int(w3.eth.getBlock(blockNumber).mixHash.hex(), 16)),
      #nonce = to_bytes(int(w3.eth.getBlock(blockNumber).nonce.hex(), 16)),
-     
  )
+
+from pyetash import hashimoto_light, mkcache_bytes
+
+# Type annotation here is to ensure we don't accidentally use strings instead of bytes.
+cache_by_epoch: 'OrderedDict[int, bytearray]' = OrderedDict() #here we cache by epoch order
+CACHE_MAX_ITEMS = 10 #and limit the items to 10
+
+def get_cache(block_number: int) -> bytes:
+        epoch_index = block_number // EPOCH_LENGTH #this is where we get the block number
+        # Get the cache if already generated, marking it as recently used
+        if epoch_index in cache_by_epoch:
+            c = cache_by_epoch.pop(epoch_index)  # pop and append at end
+            cache_by_epoch[epoch_index] = c
+            return c
+    # Generate the cache if it was not already in memory
+    # Simulate requesting mkcache by block number: multiply index by epoch length
+         c = mkcache_bytes(epoch_index * EPOCH_LENGTH)
+        cache_by_epoch[epoch_index] = c #stores the cash bytes generated
+    # Limit memory usage for cache
+         if len(cache_by_epoch) > CACHE_MAX_ITEMS: #this is related to the lenght 
+        cache_by_epoch.popitem(last=False)  # remove last recently accessed
+        #ref line88
+        return c
+#now we will write the check proof of work funtion. We need here to check if the data of the blocks is according to the requirements
+def check_pow(block_number: int,
+              mining_hash: Hash32,
+              mix_hash: Hash32,
+              nonce: bytes,
+              difficulty: int) -> None:
+        cache = get_cache(block_number) #we get cache by block number
+        mining_output = hashimoto_light(block_number, cache, mining_hash, big_endian_to_int(nonce)) #this is the hashimoto light mining output. It takes block_number, cache, mining_hash, big_endian_to_int(nonce) and hash it
+
+        print("MIX Digest: ", mining_output[b'mix digest'])
+        print("MIX HASH:   ", w3.eth.getBlock(block_number).mixHash.hex())
+
+        print("RESULT:    ", minging_output[b'result'])
+        print("CONDITION: ", (2**256) // difficulty))
+        
+        if mining_output[b'mix digest'] != mining_hash: #this is to say that if the mining digest is not equal to the mix hash, then...
+            return False 
+        elif big_endian_to_int(mining_output[b'result']) <= (2**256 // difficulty): #to convert the result int integer and check if it meets the condition of being less or equal to 2^256 divided by the difficulty
+            return False
+        else:
+            return True #if it returns true, then all good! We could do more checks but this is enough for now. For additional checks see here https://github.com/ethereum/py-evm/blob/d553bd405bbf41a1da0c227a614baba7b43e9449/eth/consensus/pow.py
+
